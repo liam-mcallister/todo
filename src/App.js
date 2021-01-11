c840c953a9491f9aa92c9456c5854b54a01ae3e7
Liam McAllister <liammca1987@gmail.com>
2021-01-11 15:10:15 +0000

    Created header section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Header />
     </div>
   );
 }
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Todo List</h1>
+      <Search />
+    </div>
+  );
+};
+
+const Search = () => {
+  return (
+    <div>
+      <input className="search" type="text" />
+      <input className="button" type="submit" value="Do it" />
+    </div>
+  );
+};
 
 export default App;
